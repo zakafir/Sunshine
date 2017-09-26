3782c99d46fb6b96d736cd1acc6b4cfcd0aa8ffc
zakafir <zak.afir@gmail.com>
2017-09-26 11:57:50 +0100

    Adding a menu

--- app/src/main/java/com/st00/afir/sunshine/MainActivity.java
+++ app/src/main/java/com/st00/afir/sunshine/MainActivity.java
@@ -2,6 +2,9 @@ package com.st00.afir.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Add a menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_search:
+                Toast.makeText(getApplicationContext(),"Search selected",Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
